41e1cdb9dc328ea6f01312eef953a281ddd4abcd
Bill-GD <geometrydashbill@gmail.com>
2026-05-12 14:17:19 +0700

    add create room endpoint
    remove room startedAt

--- src/modules/room/room.module.ts
+++ src/modules/room/room.module.ts
@@ -1,10 +1,12 @@
 import { PrismaService } from '@/services/prisma.service';
 import { Module } from '@nestjs/common';
+import { OtpService } from './otp.service';
 import { RoomController } from './room.controller';
+import { RoomGateway } from './room.gateway';
 import { RoomService } from './room.service';
 
 @Module({
   controllers: [RoomController],
-  providers: [RoomService, PrismaService],
+  providers: [RoomService, PrismaService, RoomGateway, OtpService],
 })
 export class RoomModule {}
